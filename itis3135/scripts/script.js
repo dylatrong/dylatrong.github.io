991a0f56da963e3520f850ea5e0c04d11465162e
Dylan Truong <133072202+dylatrong@users.noreply.github.com>
2025-08-31 15:41:22 -0400

    Created ITIS 3135 website pages

---
+++ itis3135/scripts/script.js
@@ -0,0 +1,5 @@
+// script.js for ITIS-3135
+console.log(" ITIS-3135 script loaded successfully. ");
+
+// You can add more dynamic functionality here later.
+// For now, this confirms the script is linked correctly.
